123cd3faaf02f90648aa6515f56ea4417265588f
Umar Al Mukhtar <umar1868807@gmail.com>
2025-07-17 14:21:09 +0530

    add product model

--- invProject/invProject/settings.py
+++ invProject/invProject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'invApp.apps.InvappConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
